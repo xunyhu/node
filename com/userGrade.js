c63028142d4cfad8e7c757b7fae97409d2dc2170
HuRui <1194547767@qq.com>
2018-08-30 02:12:21 +0800

    添加星探页面

---
+++ com/userGrade.js
@@ -0,0 +1,110 @@
+var userGrade = {
+	grade:[
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-1-10.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-11-20.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-21-30.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-31-40.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-41-50.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-51-60.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-61-70.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-71-80.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-81-90.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-91-100.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-101-120.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-101-120.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-121-140.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-121-140.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-141-160.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-141-160.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-161-180.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-161-180.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-181-200.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-181-200.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-201-220.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-201-220.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-221-240.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-221-240.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-241-260.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-241-260.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-261-280.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-261-280.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-281-300.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-281-300.png"
+	],
+	gradeImg:[
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-1-10.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-11-20.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-21-30.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-31-40.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-41-50.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-51-60.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-61-70.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-71-80.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-81-90.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-91-100.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-101-120.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-101-120.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-121-140.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-121-140.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-141-160.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-141-160.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-161-180.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-161-180.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-181-200.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-181-200.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-201-220.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-201-220.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-221-240.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-221-240.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-241-260.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-241-260.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-261-280.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-261-280.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-281-300.png",
+		"https://h5.tudouni.doubozhibo.com/m/img/usergrade/icon-grade-img-281-300.png"
+	]
+};
+
+var gradeNum = function(value) {
+	var index = 0;
+	if(!isNaN(value)) {
+		if(value < 1) {
+			value = 1;
+		} else if(value > 300) {
+			value = 300;
+		}
+		if(value % 10 == 0) {
+			value -= 1;
+		}
+		index = Math.floor(value / 10);
+	}
+	return index;
+};
+
+var binding = function(el, obj, type) {
+	var index = gradeNum(obj.value);;
+	
+	if(type == "img") {
+		el.style.backgroundImage = "url("+ userGrade.gradeImg[index] +")";
+	} else {
+		el.style.backgroundImage = "url("+ userGrade.grade[index] +")";
+	}
+}
+
+Vue.directive("usergrade", {
+	bind: function(el, obj) {
+		binding(el, obj);
+	},
+	update: function(el, obj){
+		binding(el, obj);
+	}
+});
+
+Vue.directive("usergradeimg", {
+	bind: function(el, obj) {
+		binding(el, obj, "img");
+	},
+	update: function(el, obj){
+		binding(el, obj, "img");
+	}
+});
