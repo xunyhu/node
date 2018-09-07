4bb7011c8ca181e5970946249296627c538cf5c7
HuRui <1194547767@qq.com>
2018-09-07 19:05:06 +0800

    增加courtship

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -5,6 +5,7 @@ var minifycss = require('gulp-minify-css');
 var babel = require('gulp-babel');
 var es2015 = require('babel-preset-es2015');
 var htmlmin = require('gulp-htmlmin');
+var smushit = require('gulp-smushit');
 
 gulp.task('uglifyhtml', function () {
     var options = {
@@ -30,6 +31,12 @@ gulp.task('uglifycss',function(){
 	gulp.src('../css/*.css')
 	.pipe(minifycss())
 	.pipe(gulp.dest('../dist/css'))
+});
+
+gulp.task('uglifyimg', function(){ 
+    gulp.src('../img/912/*')
+        .pipe(smushit())
+        .pipe(gulp.dest('../dist/img/912'))
 });
 
 gulp.task('ugfile', ['uglifyhtml', 'uglifyjs', 'uglifycss']);
